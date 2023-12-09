95aad0007c31a4d689a88b6d8255417edb3ab92e
VictorMedellin7 <medellinmezav@gmail.com>
2023-12-08 23:53:47 -0700

    TODO REALIZADO

--- app-reservas/components/searchModal.js
+++ app-reservas/components/searchModal.js
@@ -17,7 +17,7 @@ export const Search = ({ navigation }) => {
         const response = await axios.get('http://192.168.1.64:6969/api/munis');
         setSearchResults(response.data.munis);
       } catch (error) {
-        console.error('Error al obtener datos:', error);
+        console.error('Error al obtener datos XD:', error);
       }
     };
 
